3b747297c244f0ab3b1bfd9ce9329ced3abf4729
Gray Manley <gray.manley@gmail.com>
2011-11-18 00:48:30 -0600

    Update gems including rails to 3.1.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Soshigal::Application.config.session_store :cookie_store, key: '_picawing_session'
+Soshigal::Application.config.session_store :cookie_store, key: '_soshigal_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
