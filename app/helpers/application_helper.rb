d42aa9daedd6be8c52347444ead2c03400ce5b0f
Gray Manley <gray.manley@gmail.com>
2011-12-05 22:29:19 -0600

    Use twitter bootstrap alerts for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# encoding: UTF-8
 module ApplicationHelper
+
+  def flash_messages
+    flash.select{|name, msg| msg.is_a?(String)}.each do |name, msg|
+      haml_tag :div, class: "alert-message #{name} fade in", data: {alert: 'alert'} do
+        haml_tag 'a.close', '×', href: '#'
+        haml_tag :p, msg
+      end
+    end
+  end
 end
