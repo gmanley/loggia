8a0cef864c7efeb03e13872bf641893059740df9
Gray Manley <gray.manley@gmail.com>
2012-01-02 13:53:01 -0600

    Tweaks example s3 settings.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,16 @@
 require 'carrierwave/processing/mini_magick'
 CarrierWave.configure do |config|
 # Example settings for s3/cloudfront
+#   config.storage = :fog
 #   config.fog_credentials = {
 #     :provider               => 'AWS',
 #     :aws_access_key_id      => 'xxx',
 #     :aws_secret_access_key  => "yyy",
-#     :region                 => 'us-west-1'
 #   }
-#   config.fog_directory  = 'uploads'
+#   config.fog_directory  = 'bucket_name'
 #   config.fog_host = 'http://cdn.example.com'
-  config.storage :file # Can also be overwritten in uploaders
+#   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+  config.storage :file # Can be overridden in uploaders
 end
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 MiniMagick.processor = :gm
