4195cd6f8b1034ad55c82429e29cf5071a4f39f1
Gray Manley <gray.manley@gmail.com>
2012-01-02 15:26:55 -0600

    Add more specs for categories

--- spec/acceptance/manage_categories_spec.rb
+++ spec/acceptance/manage_categories_spec.rb
@@ -1,24 +1,73 @@
 require 'acceptance_helper'
 
-feature 'Admin creates a category' do
+feature 'Admin creates categories' do
   let(:admin) { Fabricate(:admin) }
-  let(:category) { Fabricate(:category) }
+  let(:root_category) { Fabricate.build(:category) }
+  let(:child_category) { Fabricate.build(:category) }
 
   background do
-    visit homepage
     sign_in admin
   end
 
-  scenario 'that is a top level parent' do
+  scenario 'root category' do
+
     visit homepage
 
     click_link 'New Category'
     within('form#new_category') do
-      fill_in 'Title', with: category.title
-      fill_in 'Description', with: category.description
+      fill_in 'Title', with: root_category.title
+      fill_in 'Description', with: root_category.description
     end
     click_button 'Save'
 
     page.should have_content 'Category was successfully created.'
+  end
+
+  scenario 'child category' do
+    create_root_category(root_category)
+
+    visit homepage
+
+    click_link root_category.title
+    within('form#new_category') do
+      fill_in 'Title', with: child_category.title
+      fill_in 'Description', with: child_category.description
+    end
+    click_button 'Create'
+  end
+end
+
+
+feature 'Admin destroys categories' do
+  let(:admin) { Fabricate(:admin) }
+  let(:root_category) { Fabricate.build(:category) }
+  let(:child_category) { Fabricate.build(:category) }
+
+  background do
+    sign_in admin
+  end
+
+  scenario 'root category' do
+    create_root_category(root_category)
+
+    visit homepage
+
+    click_link root_category.title
+
+    click_link 'Destroy'
+
+    page.should have_content 'Category was succesfully destroyed.'
+  end
+
+  scenario 'child category' do
+    create_child_category(root_category, child_category)
+
+    visit homepage
+
+    click_link root_category.title
+
+    click_link 'Destroy'
+
+    page.should have_content 'Category was succesfully destroyed.'
   end
 end
--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -11,6 +11,31 @@ module HelperMethods
       click_button "Sign in"
     end
   end
+
+  def create_root_category(root_category)
+    visit homepage
+
+    click_link 'New Category'
+    within('form#new_category') do
+      fill_in 'Title', with: root_category.title
+      fill_in 'Description', with: root_category.description
+    end
+    click_button 'Save'
+  end
+
+
+  def create_child_category(root_category, child_category)
+    create_root_category(root_category)
+
+    visit homepage
+
+    click_link root_category.title
+    within('form#new_category') do
+      fill_in 'Title', with: child_category.title
+      fill_in 'Description', with: child_category.description
+    end
+    click_button 'Create'
+  end
 end
 
 RSpec.configuration.include(HelperMethods, type: :request)
