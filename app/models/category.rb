a55d47e6d810de250b93d09214471ff4317f6364
Gray Manley <gray.manley@gmail.com>
2012-01-13 01:39:43 -0600

    Cache category thumb

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,6 +6,7 @@ class Category
   field :title, type: String
   field :description, type: String
   field :hidden, type: Boolean, default: false
+  field :thumbnail_url, type: String
 
   slug :title
 
@@ -13,17 +14,19 @@ class Category
   references_many :child_categories, class_name: self.name, foreign_key: :parent_category_id, inverse_of: :parent_category
   referenced_in :parent_category, class_name: self.name, inverse_of: :child_categories, index: true
 
+  before_save :set_thumbnail_url
+
   scope :roots, where(parent_category_id: nil)
 
-  def thumbnail_url
+  def set_thumbnail_url
     if albums.empty? or albums.with_images.empty?
       if child_categories.empty? or child_categories.all? {|c| c.albums.empty? or c.albums.with_images.empty? }
-        "/assets/placeholder.png"
+        self.thumbnail_url = '/assets/placeholder.png'
       else
-        child_categories.select { |cc| cc.albums.with_images.count > 0 }.sample.thumbnail_url
+        self.thumbnail_url = child_categories.select { |cc| cc.albums.with_images.count > 0 }.sample.thumbnail_url
       end
     else
-     albums.with_images.sample.thumbnail_url
+     self.thumbnail_url = albums.with_images.sample.thumbnail_url
     end
   end
 end
--- lib/tasks/soshigal.rake
+++ lib/tasks/soshigal.rake
@@ -6,12 +6,18 @@ namespace :soshigal do
     end
   end
 
-  desc 'Recache all album thumbnails'
+  desc 'Recache album and category thumbnails'
   task :recache_thumbnails => :environment do
     Album.skip_callback(:save, :before, :set_thumbnail_url)
     Album.all.each do |album|
       album.set_thumbnail_url
       album.save
+    end
+
+    Category.skip_callback(:save, :before, :set_thumbnail_url)
+    Category.all.each do |category|
+      category.set_thumbnail_url
+      category.save
     end
   end
 end
