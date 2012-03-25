08291c4c59510a1b82da4f6e0ac4860f5ebcdcdd
Gray Manley <gray.manley@gmail.com>
2012-03-24 21:54:02 -0500

    Fix set_thumbnail_url method on category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,11 @@
 class Category < Container
 
+  def album_descendants
+    Album.where(:parent_ids => self.id)
+  end
+
   def set_thumbnail_url
-    descendants_with_images = descendants.with_images
+    descendants_with_images = album_descendants.with_images
     unless descendants_with_images.empty?
       thumbnail_url = descendants_with_images.sample.thumbnail_url
       update_attribute(:thumbnail_url, thumbnail_url)
