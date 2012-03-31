914701347ec85ba8b40efb419c7c0d0972e0a893
Gray Manley <gray.manley@gmail.com>
2012-03-31 00:46:08 -0500

    Some tweaks to flash helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 # encoding: UTF-8
 module ApplicationHelper
 
+  BOOTSTRAP_FLASH_CLASS = {
+    alert:   'warning',
+    notice:  'info',
+  }
+
+  def bootstrap_flash_class(type)
+    BOOTSTRAP_FLASH_CLASS[type] || type.to_s
+  end
+
   def flash_messages
-    flash.select{|name, msg| msg.is_a?(String)}.each do |name, msg|
-      haml_tag :div, class: "alert #{name} fade in", data: {alert: 'alert'} do
-        haml_tag 'a.close', '×', data: {dismiss: 'alert'}
-        haml_concat(msg)
-      end
+    flash.each do |type, message|
+      flash_message(type, message) if message.is_a?(String)
     end
   end
 
@@ -27,6 +33,14 @@ module ApplicationHelper
   end
 
   private
+  def flash_message(type, message)
+    haml_tag :div, class: "alert alert-#{bootstrap_flash_class(type)} fade in" do
+      haml_tag 'a.close', '×', data: {dismiss: 'alert'}
+      haml_concat(message)
+    end
+  end
+
+
   def breadcrumb(text, link = nil)
     if link
       haml_tag :li do
