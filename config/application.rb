a6a73a8629ef3a27d61aeeabb80afbea078355ab
Gray Manley <gray.manley@gmail.com>
2012-09-22 21:05:42 -0500

    Reorganize lib folder and autoload paths.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups(assets: %w(development test))) if defined?(Bundler
 module Soshigal
   class Application < Rails::Application
     # Add sidekiq workers to autoloaded files
-    config.autoload_paths += %W(#{config.root}/app/workers #{config.root}/lib/shared)
+    config.autoload_paths += %W(#{config.root}/app/workers #{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
