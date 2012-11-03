4ea35af0099c6df2bf78fbc86c33f6796645a65e
Gray Manley <gray.manley@gmail.com>
2012-11-03 13:06:14 -0500

    Add text if no comments and move to helper.

---
+++ app/helpers/comments_helper.rb
@@ -0,0 +1,12 @@
+module CommentsHelper
+
+  def comments_list_for(resource)
+    haml_tag 'ol.unstyled#comments-list' do
+      render resource.comments
+
+      if resource.comments.empty?
+        haml_tag 'i.muted', 'No Comments'
+      end
+    end
+  end
+end
