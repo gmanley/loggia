ee56724151e8303d33acc419fb7c716083aa58e6
Gray Manley <gray.manley@gmail.com>
2013-03-24 22:31:16 -0500

    Minor cleanup to archive model.

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -9,30 +9,17 @@ class Archive < ActiveRecord::Base
 
   def archive_album
     return file.url unless outdated?
-
     file.remove!
-
     update_attributes(processing: true)
 
     temp_directory = Dir.mktmpdir
     zip_path = File.join(temp_directory, archivable.to_s + '.zip')
 
     Zip::Archive.open(zip_path, Zip::CREATE) do |zip|
-      archivable.sources.each do |source|
-        begin
-          zip.add_dir(source.name)
-        rescue Zip::Error
-          Rails.logger.warn("Error adding #{source.name} to archive #{id}.")
-        end
-      end
+      archivable.sources.each {|source| add_source(zip, source) }
 
-      images = archivable.images.includes(:source)
-      images.each do |image_record|
-        if source = image_record.source
-          add_image(zip, image_record, source.name)
-        else
-          add_image(zip, image_record)
-        end
+      archivable.images.includes(:source).each do |image_record|
+        add_image(zip, image_record, image_record.source.try(:name))
       end
     end
 
@@ -52,6 +39,12 @@ class Archive < ActiveRecord::Base
   end
 
   private
+  def add_source(zip, source)
+    zip.add_dir(source.name)
+  rescue Zip::Error
+    Rails.logger.warn("Error adding #{source.name} to archive #{id}.")
+  end
+
   def add_image(zip, image_record, parent_path = nil)
     image_file = image_record.image.cached_master
     zip_path = File.join(*[parent_path, image_file.filename].compact)
