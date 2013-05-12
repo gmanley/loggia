18e5592475e937adddcebd357ef3fda754ae5073
Gray Manley <gray.manley@gmail.com>
2013-05-12 15:14:28 -0700

    Remove unused controller.

--- app/controllers/sources_controller.rb
+++
@@ -1,8 +0,0 @@
-class SourcesController < ApplicationController
-
-  def show
-    @source = Source.find(params[:id])
-    @images = @source.images.page(params[:page])
-    authorize!(:show, Source)
-  end
-end
