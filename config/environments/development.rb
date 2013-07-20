394c8116cf7d974e6662e3af5aeb05987eb27c8c
Gray Manley <gray.manley@gmail.com>
2013-07-19 21:03:41 -0500

    Add middleware to speed up dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,8 @@ Soshigal::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  # See lib/middleware/turbo_dev.rb
+  config.middleware.insert 0, Middleware::TurboDev
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
