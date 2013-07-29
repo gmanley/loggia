90ea1f94a7a37d498d5179954dd890ae6de7de59
Gray Manley <gray.manley@gmail.com>
2013-07-28 23:56:24 -0500

    Fix asset precompilation.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Soshigal::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile << Proc.new { |path| !%w(.js .css).include?(File.extname(path)) }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
